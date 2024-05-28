e667a0e1910f1fc3b4aa3ca963a634f316337ac6
joao-vtr-oliveira <joao.vtr.oliveira@outlook.com>
2024-05-28 13:57:53 -0300

    chores: add the name field

--- src/types/marsLocationType.ts
+++ src/types/marsLocationType.ts
@@ -1,3 +1,4 @@
 export type marsLocationType = {
+  name: string;
   code: string;
 };
--- src/utils/marsDeliveryLocations.ts
+++ src/utils/marsDeliveryLocations.ts
@@ -1,6 +1,6 @@
 import { marsLocationType } from '@/types/marsLocationType';
 
 export let marsDeliveryLocations: marsLocationType[] = [
-  {code: '1234'},
-  {code: '1307'}
+  {name: 'Phoenix Refinery', code: '1234'},
+  {name: 'Dokkaebi OS LTDA', code: '1307'}
 ];
