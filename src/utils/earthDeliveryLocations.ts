72e7b71e5c6812d9ec3ad49e286011f521cc90d0
joao-vtr-oliveira <joao.vtr.oliveira@outlook.com>
2024-05-28 10:35:52 -0300

    feat: creat an array for each planet type

---
+++ src/utils/earthDeliveryLocations.ts
@@ -0,0 +1,6 @@
+import { earthLocationType } from '@/types/earthLocationType';
+
+export let earthDeliveryLocations: earthLocationType[] = [
+  {country: 'Brasil', addressLine: 'Rua padre toledo', city: 'Betim', state: 'Minas Gerais', zipCode: '32.676-048'},
+  {country: 'Canadá', addressLine: 'ST Washington', city: 'Red Lake', state: 'Ontário', zipCode: 'P0V 1L0'},
+];
---
+++ src/utils/marsDeliveryLocations.ts
@@ -0,0 +1,6 @@
+import { marsLocationType } from '@/types/marsLocationType';
+
+export let marsDeliveryLocations: marsLocationType[] = [
+  {code: '1234'},
+  {code: '1307'}
+];
