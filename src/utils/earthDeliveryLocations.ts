0f481f07c04e238032005061f35d50f49452a4f0
joao-vtr-oliveira <joao.vtr.oliveira@outlook.com>
2024-05-28 19:07:23 -0300

    chores: add one more location for tests

--- src/utils/earthDeliveryLocations.ts
+++ src/utils/earthDeliveryLocations.ts
@@ -3,4 +3,5 @@ import { earthLocationType } from '@/types/earthLocationType';
 export let earthDeliveryLocations: earthLocationType[] = [
   {name: 'JVDO LTDA', country: 'Brasil', addressLine: 'Rua padre toledo', city: 'Betim', state: 'Minas Gerais', zipCode: '32.676-048'},
   {name: 'Mei LTDA', country: 'Canadá', addressLine: 'ST Washington', city: 'Red Lake', state: 'Ontário', zipCode: 'P0V 1L0'},
+  {name: 'Pyke Sea Food', country: 'Alaska', addressLine: 'N/A', city: 'Sangue vermelho', state: 'Aguas de sentina', zipCode: '000.000-00'}
 ];
