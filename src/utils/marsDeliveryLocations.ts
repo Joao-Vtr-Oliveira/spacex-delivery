118294a05dd1d33ef01d966b41e4b1a7f8f0d9d6
joao-vtr-oliveira <joao.vtr.oliveira@outlook.com>
2024-05-28 11:05:55 -0300

    chores: add the ID in the array base

--- src/utils/marsDeliveryLocations.ts
+++ src/utils/marsDeliveryLocations.ts
@@ -1,6 +1,6 @@
 import { marsLocationType } from '@/types/marsLocationType';
 
 export let marsDeliveryLocations: marsLocationType[] = [
-  {code: '1234'},
-  {code: '1307'}
+  {id: 0, code: '1234'},
+  {id: 1, code: '1307'}
 ];
