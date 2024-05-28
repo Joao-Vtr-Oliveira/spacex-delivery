7d14e398a5efcef733bb38431a10aa97e68e7ba7
joao-vtr-oliveira <joao.vtr.oliveira@outlook.com>
2024-05-28 10:37:00 -0300

    chores: add some logs for tests

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,17 @@
 'use client'
 
+import { MarsContext } from '@/contexts/marsContext';
+import { EarthContext } from '@/contexts/earthContext'
 import { Card, CardHeader } from '@chakra-ui/react'
+import { useContext } from 'react'
 
 export default function Home() {
+  
+  const earthContext = useContext(EarthContext);
+  const marsContext = useContext(MarsContext);
+  console.log(earthContext?.locations);
+  console.log(marsContext?.locations);
+
   return (
     <Card>
       <CardHeader>
