34d3fd8fc287fcee160676d78f99a5c98f2b0c42
joao-vtr-oliveira <joao.vtr.oliveira@outlook.com>
2024-05-28 11:54:56 -0300

    chores: add a test page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,74 @@
 'use client';
 
+import { EarthContext } from '@/contexts/earthContext';
 import { MarsContext } from '@/contexts/marsContext';
-import { Button, Card, CardHeader } from '@chakra-ui/react';
+import { Box, Button, Card, CardBody, CardHeader } from '@chakra-ui/react';
 import { useContext } from 'react';
 
 export default function Home() {
-  const marsContext = useContext(MarsContext);
+	const marsContext = useContext(MarsContext);
+	const earthContext = useContext(EarthContext);
 
-  if (!marsContext) {
-    throw new Error('useMarsContext must be used within a MarsProvider');
-  }
+	if (!marsContext || !earthContext) {
+		throw new Error('useMarsContext must be used within a MarsProvider. Or error in earthContext.');
+	}
 
-  const { locations, addLocation, deleteLocation } = marsContext;
+	// const { locations, addLocation, deleteLocation } = marsContext;
 
-  console.log(locations); // Deve logar as localizações de Marte
+	console.log(marsContext.locations);
+	console.log(earthContext.locations);
 
-  return (
-    <Card>
-      <CardHeader>
-        <h1>Hello Mars!</h1>
-        <ul>
-          {locations.map((location, index) => (
-            <li key={index}>
-              ID: {location.id}, Code: {location.code}
-              <Button size={'sm'} onClick={() => deleteLocation(index)}>Delete</Button>
-            </li>
-          ))}
-        </ul>
-        <Button size={'sm'} onClick={() => addLocation('NEW_CODE')}>
-          Add New Location
-        </Button>
-      </CardHeader>
-    </Card>
-  );
+	return (
+		<Box>
+			<Card>
+				<CardHeader>
+					<h1>Hello Mars!</h1>
+				</CardHeader>
+				<CardBody>
+					<ul>
+						{marsContext.locations.map((location, index) => (
+							<li key={index}>
+								ID: {location.id}, Code: {location.code}
+								<Button size={'sm'} onClick={() => marsContext.deleteLocation(index)}>
+									Delete
+								</Button>
+							</li>
+						))}
+					</ul>
+					<Button size={'sm'} onClick={() => marsContext.addLocation('NEW_CODE')}>
+						Add New Location
+					</Button>
+				</CardBody>
+			</Card>
+			<Card>
+				<CardHeader>
+					<h1>Hello Earth!</h1>
+				</CardHeader>
+				<CardBody>
+					<ul>
+						{earthContext.locations.map((location, index) => (
+							<li key={index}>
+								ID: {location.id}, Address: {location.addressLine}
+								<Button size={'sm'} onClick={() => earthContext.deleteLocation(index)}>Delete</Button>
+							</li>
+						))}
+					</ul>
+					<Button
+            size={'sm'}
+						onClick={() =>
+							earthContext.addLocation({
+								country: 'New Country',
+								addressLine: 'New Address',
+								city: 'New City',
+								state: 'New State',
+								zipCode: 'New Zip',
+							})
+						}
+					>
+						Add New Location
+					</Button>
+				</CardBody>
+			</Card>
+		</Box>
+	);
 }
