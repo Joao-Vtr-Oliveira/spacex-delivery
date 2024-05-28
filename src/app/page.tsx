c7f1b0ee4e0bc3559d3c083d384511cb67dd66f1
joao-vtr-oliveira <joao.vtr.oliveira@outlook.com>
2024-05-28 11:06:38 -0300

    chores: change the app page for tests

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,36 @@
-'use client'
+'use client';
 
 import { MarsContext } from '@/contexts/marsContext';
-import { EarthContext } from '@/contexts/earthContext'
-import { Card, CardHeader } from '@chakra-ui/react'
-import { useContext } from 'react'
+import { Button, Card, CardHeader } from '@chakra-ui/react';
+import { useContext } from 'react';
 
 export default function Home() {
-  
-  const earthContext = useContext(EarthContext);
   const marsContext = useContext(MarsContext);
-  console.log(earthContext?.locations);
-  console.log(marsContext?.locations);
+
+  if (!marsContext) {
+    throw new Error('useMarsContext must be used within a MarsProvider');
+  }
+
+  const { locations, addLocation, deleteLocation } = marsContext;
+
+  console.log(locations); // Deve logar as localizações de Marte
 
   return (
     <Card>
       <CardHeader>
-        Hello world!
+        <h1>Hello Mars!</h1>
+        <ul>
+          {locations.map((location, index) => (
+            <li key={index}>
+              ID: {location.id}, Code: {location.code}
+              <Button size={'sm'} onClick={() => deleteLocation(index)}>Delete</Button>
+            </li>
+          ))}
+        </ul>
+        <Button size={'sm'} onClick={() => addLocation('NEW_CODE')}>
+          Add New Location
+        </Button>
       </CardHeader>
     </Card>
-  )
+  );
 }
