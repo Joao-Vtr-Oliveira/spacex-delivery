bd309309420c08407e6ab2d6127c1eac562db319
joao-vtr-oliveira <joao.vtr.oliveira@outlook.com>
2024-05-29 12:14:20 -0300

    choes: change the base of locations

--- src/utils/earthDeliveryLocations.ts
+++ src/utils/earthDeliveryLocations.ts
@@ -2,6 +2,6 @@ import { earthLocationType } from '@/types/earthLocationType';
 
 export let earthDeliveryLocations: earthLocationType[] = [
   {id: 1716984644361, name: 'JVDO LTDA', country: 'Brasil', addressLine: 'Rua padre toledo', city: 'Betim', state: 'Minas Gerais', zipCode: '32.676-048'},
-  {id: 1716984655988, name: 'Mei LTDA', country: 'Canadá', addressLine: 'ST Washington', city: 'Red Lake', state: 'Ontário', zipCode: 'P0V 1L0'},
-  {id: 1716984666054, name: 'Pyke Sea Food', country: 'Alaska', addressLine: 'N/A', city: 'Sangue vermelho', state: 'Aguas de sentina', zipCode: '000.000-00'}
+  {id: 1716984655988, name: 'Mei LTDA', country: 'Canada', addressLine: 'ST Washington', city: 'Red Lake', state: 'Ontário', zipCode: 'P0V 1L0'},
+  {id: 1716984666054, name: 'Pyke Sea Food', country: 'Brasil', addressLine: 'Faria Lima', city: 'SP', state: 'SP', zipCode: '01451-000'}
 ];
