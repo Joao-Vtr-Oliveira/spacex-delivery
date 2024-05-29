1b5f38ac7554dd16a60151d23db16f5a57b9febb
joao-vtr-oliveira <joao.vtr.oliveira@outlook.com>
2024-05-29 09:59:14 -0300

    feat: add the edit page

---
+++ src/app/earth/edit/[id]/page.tsx
@@ -0,0 +1,17 @@
+
+import EditEarthLocationPage from '@/pagesComponents/EditEarthLocationPage';
+import { PageParams } from '@/types/pageParams';
+import { Box } from '@chakra-ui/react';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+	title: 'Edit Earth Location',
+};
+
+export default function AddEarthLocation({ params }: PageParams) {
+	return (
+		<Box className='flex min-h-screen h-screen w-screen flex-col items-center justify-center p-24'>
+			<EditEarthLocationPage params={params} />
+		</Box>
+	);
+}
---
+++ src/pagesComponents/EditEarthLocationPage.tsx
@@ -0,0 +1,125 @@
+'use client';
+
+import { EarthContext } from '@/contexts/earthContext';
+import { earthLocationType } from '@/types/earthLocationType';
+import { PageParams } from '@/types/pageParams';
+import { checkEarthFields } from '@/utils/checkFields';
+import { Box, Button, Heading, Input } from '@chakra-ui/react';
+import { useRouter } from 'next/navigation';
+import { useContext, useState } from 'react';
+
+const EditEarthLocationPage = ({ params }: PageParams) => {
+	const earthContext = useContext(EarthContext);
+
+	if (!earthContext) return 'Error';
+
+	const actualLocation = earthContext.locations.filter(
+		(item) => item.id === Number(params.id)
+	)[0];
+	console.log(actualLocation);
+
+	const [location, setLocation] = useState<earthLocationType>(actualLocation);
+
+	const { push } = useRouter();
+
+	const updateLocation = () => {
+		const check = checkEarthFields(location);
+		if (!check) return alert('Please, fill all the fields');
+		earthContext.updateLocation(location.id, location);
+		push('/');
+	};
+
+	return (
+		<Box>
+			<Heading textAlign='center' mb={10}>
+				Edit Earth Location:
+			</Heading>
+			<Box mb={5}>
+				<Heading textAlign='center' mb={3} size='md'>
+					Name
+				</Heading>
+				<Input
+					value={location.name}
+					textAlign='center'
+					onChange={(e) => setLocation({ ...location, name: e.target.value })}
+					required
+				/>
+			</Box>
+			<Box mb={5}>
+				<Heading textAlign='center' mb={3} size='md'>
+					Address line
+				</Heading>
+				<Input
+					value={location.addressLine}
+					textAlign='center'
+					onChange={(e) =>
+						setLocation({ ...location, addressLine: e.target.value })
+					}
+					required
+				/>
+			</Box>
+			<Box mb={5}>
+				<Heading textAlign='center' mb={3} size='md'>
+					Country
+				</Heading>
+				<Input
+					value={location.country}
+					textAlign='center'
+					onChange={(e) =>
+						setLocation({ ...location, country: e.target.value })
+					}
+					required
+				/>
+			</Box>
+			<Box mb={5}>
+				<Heading textAlign='center' mb={3} size='md'>
+					State
+				</Heading>
+				<Input
+					value={location.state}
+					textAlign='center'
+					onChange={(e) => setLocation({ ...location, state: e.target.value })}
+					required
+				/>
+			</Box>
+			<Box mb={5}>
+				<Heading textAlign='center' mb={3} size='md'>
+					City
+				</Heading>
+				<Input
+					value={location.city}
+					textAlign='center'
+					onChange={(e) => setLocation({ ...location, city: e.target.value })}
+					required
+				/>
+			</Box>
+			<Box mb={5}>
+				<Heading textAlign='center' mb={3} size='md'>
+					Zip Code
+				</Heading>
+				<Input
+					value={location.zipCode}
+					textAlign='center'
+					onChange={(e) =>
+						setLocation({ ...location, zipCode: e.target.value })
+					}
+					required
+				/>
+			</Box>
+			<Box display='flex' justifyContent='center' gap={3}>
+				<Button
+					colorScheme='green'
+					isDisabled={!checkEarthFields(location)}
+					onClick={updateLocation}
+				>
+					Update Location
+				</Button>
+				<Button colorScheme='red' onClick={() => push('/')}>
+					Cancel
+				</Button>
+			</Box>
+		</Box>
+	);
+};
+
+export default EditEarthLocationPage;
